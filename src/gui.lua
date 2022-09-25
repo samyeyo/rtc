32d25996d06d20b9af2296a1f86cdf90d15056a0
Samir Tine <samir.tine@luart.org>
2022-09-25 09:55:24 +0200

    [New] GUI frontend for rtc

---
+++ src/gui.lua
@@ -0,0 +1,131 @@
+local File =  embed == nil and sys.File  or embed.File
+local icon
+
+local win = ui.Window("rtc - Lua script to executable compiler", "fixed", 400, 260, "fixed")
+win:loadicon(File("img/rtc.ico"))
+
+local function setLabelBtn(widget, text, x, y, icon, tooltip, nobtn)
+    widget.label = ui.Label(win, text, x, y)
+    widget.x = widget.label.x + widget.label.width + (nobtn and 22 or 2)
+    widget.y = widget.label.y - 3
+    if not nobtn then
+        widget.chooseBtn = ui.Button(win, "", widget.x + widget.width + 1, widget.y-2, 24, 24)
+        widget.chooseBtn:loadicon(icon)
+        widget.chooseBtn.hastext = false
+    end
+    widget.tooltip = tooltip
+    return widget
+end
+
+local script = setLabelBtn(ui.Entry(win, "", 10, 10, win.width-130), "Main Lua script : ", 10, 26, File("img/open.ico"), "The Lua script to be run when the generated executable starts")
+local embed = setLabelBtn(ui.Entry(win, "", 10, 10, win.width-130), "Embed directory: ", 10, 52, File("img/open.ico"), "The content of the directory will be embedded in the generated executable")
+local output = setLabelBtn(ui.Entry(win, "", 10, 10, win.width-130), "Executable : ", 10, 78, File("img/open.ico"), "The content of the directory will be embedded in the generated executable", true)
+
+local group = ui.Groupbox(win, "Windows subsystem", 10, 116, 130, 90)
+local radioconsole= ui.Radiobutton(group, "Console", 10, 26)
+radioconsole.checked = true
+local radiodesktop = ui.Radiobutton(group, "Windows desktop", 10, 50)
+
+local group = ui.Groupbox(win, "Runtime library", 150, 116, 130, 90)
+local radiodynamic = ui.Radiobutton(group, "Dynamic", 26, 26)
+radiodynamic.checked = true
+local radiostatic = ui.Radiobutton(group, "Static", 26, 50)
+
+local lbl = ui.Label(win, "Executable icon", 300, 136)
+local iconBtn = ui.Button(win, "", 300+lbl.width/2-16, lbl.y + lbl.height+4, 32, 32)
+iconBtn.hastext = false
+
+local execBtn = ui.Button(win, "Generate executable", 134, 220)
+execBtn:loadicon(File("img/exec.ico"))
+execBtn.enabled = false
+
+function iconBtn:onClick()
+    local f = ui.opendialog("Select an icon", false, "ICO files (*.ico)|*.ico")
+    if f ~= nil then
+        icon = f.fullpath
+        iconBtn:loadicon(f)
+    else
+        iconBtn:loadicon(nil)
+        icon = nil
+    end
+end
+
+function update_icon()
+    target = radioconsole.checked and "luart" or "wluart"
+    if icon == nil then
+        iconBtn:loadicon(File(target..".exe"))
+    end
+end
+
+radioconsole.onClick = update_icon
+radiodesktop.onClick = update_icon
+
+if #arg >= 1 and arg[1]:sub(1,1) ~= '-' then
+    script.text = arg[1]
+    if arg[2] ~= nil and arg[2]:sub(1,1) ~= '-' then
+        embed.text = arg[2]
+    end
+end
+
+function script:onChange()
+    execBtn.enabled = sys.File(script.text).exists
+    radioconsole.checked = script.text:match("(%.%w+)$") ~= ".wlua"
+    radiodesktop.checked = not radioconsole.checked
+    update_icon()
+    output.text = script.text:gsub("(%.%w+)$", ".exe")
+end
+
+function script.chooseBtn:onClick()
+    local file = ui.opendialog("Open main Lua script...", false, "Lua script (*.lua, *.wlua)|*.lua;*.wlua|Lua desktop script (*.wlua)|*.wlua|All files (*.*)|*.*")
+    if file ~= nil then
+        script.text = file.fullpath		
+    end
+end
+
+function embed.chooseBtn:onClick()
+    local dir = ui.dirdialog("Select a directory...")
+    if dir ~= nil then
+        embed.text = dir.fullpath
+    end
+end
+
+function radiostatic:onClick()
+    if self.checked and ui.confirm( "It is strongly discouraged to use the static runtime library if you are using Lua binary modules.\nThis can lead to bugs and crashes of your application.\n\nPlease confirm that you want to use the static runtime library.", "Using static LuaRT runtime") == "cancel" then
+        self.checked = false
+        radiodynamic.checked = true
+    end
+end
+
+function win:onClose()
+    sys.exit()
+end
+
+function execBtn:onClick()
+    if #embed.text > 0 and not sys.Directory(embed.text).exists then
+       ui.error("Embedded directory does not exists")
+    else
+        win:hide()
+    end
+end
+
+update_icon()
+win:center()
+win:show()
+
+while win.visible do
+    ui.update()
+end
+
+local directory = #embed.text > 0 and sys.Directory(embed.text) or nil
+
+if radiostatic.checked then
+    target = target.."-static"
+end
+
+return { 
+    sys.File(script.text),
+    directory,
+    target..".exe",
+    output.text,
+    icon
+}
---
+++ src/rtc.lua
@@ -0,0 +1,144 @@
+-- | RTc - Lua script to executable compiler
+-- | Luart.org, Copyright (c) Tine Samir 2022.
+-- | See Copyright Notice in LICENSE.TXT
+-- |---------------------------------------------------
+-- | RTc.lua | LuaRT executable compiler
+
+local zip = require "zip"
+
+local output				-- output executable filename
+local icon					-- executable icon
+local directory				-- Directory to be embedded in the executable
+local file					-- main Lua File to be executed when running the executable
+local target = "luart.exe"	-- target interpreter for console or window subsystem
+local setoutput, set_icon, static, forceconsole = false, false, false, false
+local console, gui
+
+ui = false
+
+if package.loaded["ui"] == nil then
+	console = require "console"
+	local idx = (package.loaded["embed"] == nil) and 1 or 0
+	if #arg == idx then
+		console.writecolor("brightwhite", "Lua")
+		console.writecolor('yellow', "RT")
+		print([[ 0.9.9 - Lua script to executable compiler.
+Copyright (c) 2022, Samir Tine.
+	
+usage:	rtc.exe [-s][-c][-w][-i icon][-o output] [directory] main.lua
+	
+	-s		create static executable (without LUA54.DLL dependency)
+	-c		create executable for console (default)
+	-w		create executable for Windows desktop
+	-i icon		set executable icon (expects an .ico file)
+	-o output	set executable name to 'output'
+	directory	the content of the directory to be embedded in the executable
+	main.lua   	the Lua script to be executed]])
+		sys.exit()
+	end
+
+	------------------------------------| Parse commande line
+	for i, option in ipairs(arg) do
+		if setoutput then
+			output = option
+			setoutput = false
+		elseif set_icon then
+			icon = option
+			set_icon = false
+		elseif option == "-i" then
+			set_icon = true
+		elseif option == "-c" then
+			forceconsole = true
+			target = "luart.exe"
+		elseif option == "-w" then
+			target = "wluart.exe"
+		elseif option == "-o" then
+			setoutput = true
+		elseif option == "-s" then
+			static = true
+		elseif option:sub(1,1) == "-" then 
+			print("invalid option "..option)
+			sys.exit(-1)
+		elseif directory == nil and not sys.File(option).exists then
+			directory = sys.Directory(option)
+			if not directory.exists then
+				error("cannot find file "..option)
+			end
+		else 
+			file = sys.File(option)
+			if not file.exists then
+				error("cannot find file "..option)
+			end
+		end
+	end
+else
+	ui = require "ui"
+	local result = require("gui")
+	file, directory, target, output, icon = table.unpack(result)
+end
+
+if file == nil then
+	error("no input file")
+end
+
+if file.extension == ".wlua" and target == "luart.exe" and not forceconsole then
+	target = "wluart.exe"
+end
+
+if static then
+	if target == "luart.exe" then
+		target = "luart-static.exe"
+	else
+		target = "wluart-static.exe"
+	end
+end
+
+result, err = load(file:open():read())
+if result == nil then
+	error(err)
+end
+
+if embed == nil then
+	target = sys.File(sys.File(arg[0]).path..target)
+else
+	target = embed.File(target)
+end
+
+if not target.exists then
+    error("Fatal error: compilation failed, check your LuaRT installation")
+end
+
+local fs = sys.tempfile("luartc_")
+local fname = sys.tempfile("luartc_main_")
+
+fname:open("write", "binary")
+fname:write('require "'..file.name:gsub(file.extension, "")..'"')
+fname:close()
+
+local z = zip.Zip(fs.fullpath, "write")
+z:write(file)
+z:write(fname, "__mainLuaRTStartup__.lua")
+
+if directory ~= nil then
+	z:write(directory)
+end
+z:close()
+
+output = sys.File(output or file.name:gsub("(%w+)$", "exe"))
+output:remove()
+target:copy(output.fullpath)
+
+if icon ~= nil then
+	seticon(output.fullpath, icon)
+end
+
+if sys.cmd('copy /b "'..output.fullpath..'"+"'..fs.fullpath..'" "'..output.fullpath..'"', true) == 1 then
+	error('Cannot write "'..output.fullpath..'" to disk')
+end
+
+if ui then
+	ui.info("Successfully compiled "..output.name)
+	sys.cmd('start explorer "'..output.path..'"')
+else
+	print(output.name)
+end
