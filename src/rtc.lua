8fd6be01ce8e01d115382d0cca646210136dcbd4
Samir Tine <samir.tine@luart.org>
2022-10-08 18:23:36 +0200

    Fixed wrong src/rtc.lua version to 1.0.0

--- src/rtc.lua
+++ src/rtc.lua
@@ -22,7 +22,7 @@ if package.loaded["ui"] == nil then
 	if #arg == idx then
 		console.writecolor("brightwhite", "Lua")
 		console.writecolor('yellow', "RT")
-		print([[ 0.9.9 - Lua script to executable compiler.
+		print([[ 1.0.0 - Lua script to executable compiler.
 Copyright (c) 2022, Samir Tine.
 	
 usage:	rtc.exe [-s][-c][-w][-i icon][-o output] [directory] main.lua
